da4c90bd7ae96a6da898765d115855fbeefe849a
yuxuan wang <2879296442@qq.com>
2020-11-29 23:11:33 +0000

    Start Route-based code splitting

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense  } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
@@ -8,25 +8,27 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 
 import Home from "./components/home";
-import About from "./components/about";
+
 import Inbox from "./components/inbox";
 import Register from "./components/register";
 import Contact from "./components/contact";
-
+const About = lazy(() => import("./components/about"));
 const App = () => {
   return (
     <BrowserRouter>
       <div>
         <Header />
         <div className="container">
-          <Switch>
-            <Route path="/about" component={About} />
-            <Route path="/register" component={Register} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/inbox/:userId" component={Inbox} />
-            <Route exact path="/" component={Home} />
-            <Redirect from="*" to="/" />
-          </Switch>
+          <Suspense fallback={<h1>Loading page....</h1>}>
+            <Switch>
+              <Route path="/about" component={About} />
+              <Route path="/register" component={Register} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/inbox/:userId" component={Inbox} />
+              <Route exact path="/" component={Home} />
+              <Redirect from="*" to="/" />
+            </Switch>
+          </Suspense>
         </div>
         <Footer />
       </div>
